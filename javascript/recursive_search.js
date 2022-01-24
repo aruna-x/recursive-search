70d34811cd5c03a5fdb539cd2d24d3217d876e4d
Aruna <aruna.evan15@gmail.com>
2022-01-24 14:34:03 -0800

    Solution

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,10 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (arr.length === 0) {
+    return false;
+  }
+  else {
+    return arr[0] === target ? true : recursiveSearch(arr.slice(1), target);
+  }
 }
 
 if (require.main === module) {
@@ -11,9 +16,39 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", recursiveSearch([3, 2, 1], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", recursiveSearch([3], 3));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", recursiveSearch([], 4));
 }
 
 module.exports = recursiveSearch;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * given an array and a target 
+ * return true if array includes target
+ * else false.
+ * 
+ * 2. tests
+ * 
+ * [], 4
+ * [3], 3
+ * 
+ * 3. pseudocode
+ * 
+ * compare target to first element.
+ * if no, pass back in target and spliced or shifted array... not very efficient (same as iteration)
+ * 
+ * 4. code (above)
+ * 5. refactor
+ * 6. optimize
+ * 
+ */
